3e33661f90eb5988e158dc41fc9855d805877fc8
AlphaBrock <linjcciam1314@gmail.com>
2022-08-25 17:36:18 +0800

    feature: 增加企业微信推送

--- main.py
+++ main.py
@@ -19,8 +19,10 @@ from loguru import logger
 from resource.apis import API
 from resource.utils import Utils, ConfigReader
 from resource.wechat import Wechat
+from resource.wework import Wework
 
 wechat = Wechat()
+wework = Wework()
 api = API()
 common = Utils()
 config = ConfigReader()
@@ -61,71 +63,89 @@ def run():
     boyBirthday = common.getBirthday(config.conf["PersonData"]["boyBirthday"], year, today)
     girlBirthday = common.getBirthday(config.conf["PersonData"]["girlBirthday"], year, today)
 
-    data = {
-        "date": {
-            "value": "{} {}".format(today, week),
-            "color": common.generateColor()
-        },
-        "city": {
-            "value": config.conf["CityInfo"]["cityName"],
-            "color": common.generateColor()
-        },
-        "weather": {
-            "value": weather,
-            "color": common.generateColor()
-        },
-        "temperature": {
-            "value": temperature,
-            "color": common.generateColor()
-        },
-        "love_day": {
-            "value": love_days,
-            "color": common.generateColor()
-        },
-        "marry_day": {
-            "value": marry_days,
-            "color": common.generateColor()
-        },
-        "note_en": {
-            "value": note_en,
-            "color": common.generateColor()
-        },
-        "note_ch": {
-            "value": note_cn,
-            "color": common.generateColor()
-        },
+    if config.conf["Wechat"]["appID"]:
+        logger.info("当前可执行微信公众号推送")
+        data = {
+            "date": {
+                "value": "{} {}".format(today, week),
+                "color": common.generateColor()
+            },
+            "city": {
+                "value": config.conf["CityInfo"]["cityName"],
+                "color": common.generateColor()
+            },
+            "weather": {
+                "value": weather,
+                "color": common.generateColor()
+            },
+            "temperature": {
+                "value": temperature,
+                "color": common.generateColor()
+            },
+            "love_day": {
+                "value": love_days,
+                "color": common.generateColor()
+            },
+            "marry_day": {
+                "value": marry_days,
+                "color": common.generateColor()
+            },
+            "note_en": {
+                "value": note_en,
+                "color": common.generateColor()
+            },
+            "note_ch": {
+                "value": note_cn,
+                "color": common.generateColor()
+            },
 
-        "pipi": {
-            "value": rainbowFart,
-            "color": common.generateColor()
-        },
+            "pipi": {
+                "value": rainbowFart,
+                "color": common.generateColor()
+            },
 
-        "lucky": {
-            "value": horoscope,
-            "color": common.generateColor()
-        },
-        "lizhi": {
-            "value": inspirationalSayings,
-            "color": common.generateColor()
-        },
-        "health": {
-            "value": healthTips,
-            "color": common.generateColor()
-        },
-        "boyBirthday": {
-            "value": boyBirthday,
-            "color": common.generateColor()
-        },
-        "girlBirthday": {
-            "value": girlBirthday,
-            "color": common.generateColor()
+            "lucky": {
+                "value": horoscope,
+                "color": common.generateColor()
+            },
+            "lizhi": {
+                "value": inspirationalSayings,
+                "color": common.generateColor()
+            },
+            "health": {
+                "value": healthTips,
+                "color": common.generateColor()
+            },
+            "boyBirthday": {
+                "value": boyBirthday,
+                "color": common.generateColor()
+            },
+            "girlBirthday": {
+                "value": girlBirthday,
+                "color": common.generateColor()
+            }
         }
-    }
-    logger.info(json.dumps(data, ensure_ascii=False))
-    wechat.senMsg(data)
+        logger.info(json.dumps(data, ensure_ascii=False))
+        wechat.senMsg(data)
+
+    if config.conf["Wework"]["WECOM_CID"]:
+        logger.info("当前可执行企业推送")
+        text = []
+        text.append("【亲爱的老婆, 你好呀!】")
+        text.append("\n")
+        text.append("今天是:  {} {}".format(today, week))
+        text.append("城市:  {}".format(config.conf["CityInfo"]["cityName"]))
+        text.append("天气:  {}".format(weather))
+        text.append("气温:  {}℃".format(temperature))
+        text.append("今天是我们恋爱的第   [{}] 天".format(love_days))
+        text.append("同时也是我们结婚的第 [{}] 天".format(marry_days))
+        text.append("距离小宝生日还有 [{}] 天".format(girlBirthday))
+        text.append("距离我的生日还有 [{}] 天".format(boyBirthday))
+        logger.info("\n".join(text))
+        wework.sendMsg(text="\n".join(text))
 
 
 if __name__ == '__main__':
     logger.add("log/WechatTempLatePusher.log", rotation="100 MB", retention='10 days', compression='zip', enqueue=True, level='INFO')
-    scheduler.add_job(func=run, trigger="cron", hour=0, minute=8)
+    scheduler.add_job(func=run, trigger="cron", hour=17, minute=34)
     scheduler.start()
